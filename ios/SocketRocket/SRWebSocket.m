3eb6a68931b48ddcfeea11197f0113140ccfdaad
tom quas <tom@patugo.com>
2014-11-24 21:09:18 +0100

    trust all certs

--- ios/SocketRocket/SRWebSocket.m
+++ ios/SocketRocket/SRWebSocket.m
@@ -569,10 +569,10 @@ static __strong NSData *CRLFCRLF;
             [SSLOptions setValue:[NSNumber numberWithBool:NO] forKey:(__bridge id)kCFStreamSSLValidatesCertificateChain];
         }
         
-#if DEBUG
+// #if DEBUG
         [SSLOptions setValue:[NSNumber numberWithBool:NO] forKey:(__bridge id)kCFStreamSSLValidatesCertificateChain];
-        NSLog(@"SocketRocket: In debug mode.  Allowing connection to any root cert");
-#endif
+        // NSLog(@"SocketRocket: In debug mode.  Allowing connection to any root cert");
+// #endif
         
         [_outputStream setProperty:SSLOptions
                             forKey:(__bridge id)kCFStreamPropertySSLSettings];
